bd51dddc353b479496f0b893c6429f43d80cb1ee
kurzum <kurzum@github.com>
2013-08-11 15:50:59 +0200

    added navigation

--- update_server.sh
+++ update_server.sh
@@ -1,5 +1,5 @@
 #update index.html
-./generate_attribution > index.html
+#./generate_attribution > index.html
 
 # update owl files:
 for f in `find . -name "*.ttl" ` ; do FILE=$(echo $f | sed 's/.ttl$//'); rapper -i turtle -o rdfxml-abbrev $f >  $FILE".owl" ; done
