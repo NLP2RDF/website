ffa0d84c5b1946d2a779202b247f0ede8bb1da7c
Martin Brümmer <der.bruemmer@gmail.com>
2014-03-11 14:28:30 +0100

    added standalone resource page, edited publish script

--- publish.sh
+++ publish.sh
@@ -1,5 +1,6 @@
 # sync run
 rsync -rav index.html nlp2rdf@www.uni-leipzig.de:/data/homewww/nlp2rdf/webdir/index.html
+rsync -rav resources.html nlp2rdf@www.uni-leipzig.de:/data/homewww/nlp2rdf/webdir/resources.html
 rsync -rav documents/ nlp2rdf@www.uni-leipzig.de:/data/homewww/nlp2rdf/webdir/documents
 rsync -rav examples/ nlp2rdf@www.uni-leipzig.de:/data/homewww/nlp2rdf/webdir/examples
 rsync -rav page/ nlp2rdf@www.uni-leipzig.de:/data/homewww/nlp2rdf/webdir/page
